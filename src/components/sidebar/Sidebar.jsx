9be572532abc06222fd403c82f733a1be642d893
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-05-18 18:44:38 +0100

    Nav bar Component Created

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
-import './sidebar.css'
+import './sidebar.css';
+import Logo from '../../assets/logo.svg';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className='aside'>
+      <a href='#home' className='nav__logo'>
+        <img src={Logo} alt=''/>
+      </a>
+
+      <nav className='nav'>
+        <div className='nav__menu'>
+          <ul className='nav__list'>
+            <li className='nav__item'>
+              <a href='' className='nav__link'></a>
+            </li>
+            <li className='nav__item'>
+              <a href='#home' className='nav__link'>
+                <i className='icon-home'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#about' className='nav__link'>
+                <i className='icon-user-following'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#services' className='nav__link'>
+                <i className='icon-briefcase'></i>
+              </a>
+            </li>
+            <li className='nav__item'>
+              <a href='#resume' className='nav__link'>
+                <i className='icon-graduation'></i>
+              </a>
+              </li>
+            <li className='nav__item'>
+              <a href='#portfolio' className='nav__link'>
+                <i className='icon-layers'></i>
+              </a>
+              </li>
+            <li className='nav__item'>
+              <a href='#contact' className='nav__link'>
+                <i className='icon-bubbles'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <footer className='nav__footer'>
+        <span className='copyright'>&copy; 2023 - 2024.</span>
+      </footer>
+    </aside>
   )
 }
 
