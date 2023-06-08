afebe4494db5e9489238a5021aa28a1909fde140
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-06-08 07:52:30 +0100

    Website Responsiveness fix

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,7 +19,6 @@ const Contact = () => {
     emailjs.sendForm('service_kdpnvqn', 'template_mf2nfwd', form.current, 'G1vx6uLDHw57tu-OV')
       .then((result) => {
           console.log(result.text);
-          console.log('message sent')
       }, (error) => {
           console.log(error.text);
       });
@@ -31,7 +30,7 @@ const Contact = () => {
       <div className='contact__container grid'>
         <div className='contact__info'>
           <h3 className='contact__title'>Let's talk about everything!</h3>
-          <p className='contact__dtails'>Don't like forms? Send me an email.</p>
+          <p className='contact__details'>Don't like forms? Send me an email.</p>
         </div>
 
         <form action='' className='contact__form' ref={form} onSubmit={sendEmail}>
--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 const HeaderSocials = () => {
   return (
     <div className='home__socials'>
-        <a href='https://www.linkedin.com/in/tolu-olopade-1b4936139/' className='home__social-link' target='_blank'>
+        <a href='https://www.linkedin.com/in/tolu-olopade-1b4936139/' rel="noopener" className='home__social-link' target='_blank'>
             <i class="fa-brands fa-linkedin"></i>
         </a>
-        <a href='https://www.github.com/tolutoon' className='home__social-link' target='_blank'>
+        <a href='https://www.github.com/tolutoon' className='home__social-link' rel="noopener" target='_blank'>
             <i class="fa-brands fa-github"></i>
         </a>
-        <a href='https://www.instagram.com/tolutoon' className='home__social-link' target='_blank'>
+        <a href='https://www.instagram.com/tolutoon' className='home__social-link' rel="noopener" target='_blank'>
             <i class='fa-brands fa-instagram'></i>
         </a>
-        <a href='https://twitter.com/tolutoon' className='home__social-link' target='_blank'>
+        <a href='https://twitter.com/tolutoon' className='home__social-link' rel="noopener" target='_blank'>
             <i class="fa-brands fa-twitter"></i>
         </a>
-        <a href='https://dribbble.com/' className='home__social-link' target='_blank'>
+        <a href='https://dribbble.com/' className='home__social-link' rel="noopener" target='_blank'>
             <i class="fa-brands fa-dribbble"></i>
         </a>
-        <a href='https://behance.com/olopadetolu' className='home__social-link' target='_blank'>
+        <a href='https://behance.com/olopadetolu' className='home__social-link' rel="noopener" target='_blank'>
             <i class="fa-brands fa-behance"></i>
         </a>
     </div>
